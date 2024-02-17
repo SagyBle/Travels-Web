9a078f764d819a7353e835ca6c0fb34e1bc0ed77
yahalomer <yahalomer@gmail.com>
2024-02-17 16:26:33 +0200

    Omer test ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { FaceIcon } from "@radix-ui/react-icons"
+
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello world!</h1>
+      <div class="container mx-auto pt-5">
+        <Alert>
+          <FaceIcon className="h-5 w-5" />
+          <AlertTitle>Oooofa Blecher!</AlertTitle>
+          <AlertDescription>
+            This is my first try with shadcnUI and tailwind on our next project
+          </AlertDescription>
+        </Alert>   
+      </div>  
     </main>
   );
 }
